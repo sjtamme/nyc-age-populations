3ea38e85ed521d76fa59422e09064eb4c92eecac
Sarah Jane Tamme <sarah.tamme@uky.edu>
2021-06-24 22:15:03 -0400

    fix dropdown menu

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ const map = L.map('map', options);
 const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-/*
-const tiles = L.tileLayer('http://{s}.tile.stamen.com/toner-background/{z}/{x}/{y}.{ext}', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: 'abcd',
-    ext: 'png'
-}).addTo(map);  */
 
 
 const newyorkLayer = $.getJSON("data/nyc_tracts.geojson", function (counties) {
